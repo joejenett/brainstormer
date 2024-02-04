81038cd007d14d195bf6dc7f6a5c2cace0205fad
joe jenett <52048847+joejenett@users.noreply.github.com>
2024-02-04 15:15:09 -0500

    changed title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<title>Nicholas Chen's blog</title>
+<title>jenett-brainstormer</title>
 <div class="md:w-[26rem] lg:w-[36rem] mx-auto md:p-0 px-5">
 	<h1>
 		Welcome to my blog! Click on a post in the sidebar to view it. Click on a paragraph to view
